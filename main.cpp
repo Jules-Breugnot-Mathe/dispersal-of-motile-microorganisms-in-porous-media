4c7bdf44ea56fbcb1b6ca311318b7f27970dc4b0
Jules Breugnot-Mathé <jules.breugnot-mathE@etu.univ-lyon1.fr>
2025-10-23 14:17:42 +0200

    organisation différente des dossiers - ajout d'une classe Domain dans Environment permettant de simuler sur un Tore par périodicité de l'environnement - optimisation de la complexité temporelle

--- main.cpp
+++ main.cpp
@@ -26,14 +26,14 @@ int main(){
     uint64_t seed = static_cast<uint64_t>(std::random_device{}());
     
     Point P(0, 0);
-    Environment E("empty", 1, 0.4);
+    Environment E("uniform_Disks", 1, 0.4);
     Mobile M(P, 1, 0, 1, 0, 0.5); // v0=10, Dr=0, Tau=1, Theta=0, mu = 0.7
 
     
-    //M.write_trajectory(E, 100, 0.01 , P, seed, "isotropic", "Trajectories.csv");
+    M.write_trajectory(E, 100, 0.01 , P, seed, "isotropic", "C:\\Users\\Jules\\Desktop\\Stage ENS 4A\\data\\Trajectories.csv");
     //cout<<"deplacement final : "<<M.getMt()<<endl;
     /*
-    double D = 0;
+    double D = 0; 
     int N = 50;
     for (int i=0 ; i<N ; i++){
         M.simulation(E, 50000, 0.01, Point(0,  0), seed+i, "isotropic");
@@ -47,7 +47,7 @@ int main(){
     //std::cout<<(M.getMt()*M.getMt())/(4*34792)<<std::endl;
 
     //M.test_exponentiality(E, 200, 0.001 , P, seed, "isotropic", "Tau_estimation_data.csv", 1);
-    M.measure_diffusivity(E, 0.1, P, "isotropic", "D_estimation_data.csv", 100000, 2000, 1000);
+    //M.measure_diffusivity(E, 0.1, P, "isotropic", "D_estimation_data.csv", 100000, 2000, 1000);
     //M.measure_displacement(E, 34792, 0.01, P, "isotropic", "measure_displacement.csv", 1000);
 
 
