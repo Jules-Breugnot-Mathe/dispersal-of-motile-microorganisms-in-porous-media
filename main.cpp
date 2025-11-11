c8f8ca341729df46a3442f27f4de2d6b75adb02d
Jules Breugnot-Mathé <jules.breugnot-mathE@etu.univ-lyon1.fr>
2025-11-11 11:02:03 +0100

    derniere modif

--- main.cpp
+++ main.cpp
@@ -38,8 +38,8 @@ int main(){
 
     M.diffusivity_function_of_tau(E3, P, "isotropic", "Diffusivity_IS_large_uniform_Squares.csv", 100, 1000, 20);
     M.diffusivity_function_of_tau(E3, P, "run_and_reverse", "Diffusivity_RR_large_uniform_Squares.csv", 100, 1000, 20);
-    M.diffusivity_function_of_tau(E4, P, "isotropic", "Diffusivity_IS_small_uniform_Squares.csv", 10, 1000, 20);
-    M.diffusivity_function_of_tau(E4, P, "run_and_reverse", "Diffusivity_RR_small_uniform_Squares.csv", 10, 1000, 20);
+    M.diffusivity_function_of_tau(E4, P, "isotropic", "Diffusivity_IS_small_uniform_Squares.csv", 100, 1000, 20);
+    M.diffusivity_function_of_tau(E4, P, "run_and_reverse", "Diffusivity_RR_small_uniform_Squares.csv", 100, 1000, 20);
     
 
 
